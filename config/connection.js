e8a944df70325d538f91fa241613f262308a439b
Santiago Castaneda Munoz <santiagocm98@hotmail.com>
2020-10-20 01:35:13 -0500

    Fixing Connection Typos

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 
-let connnection;
+let connection;
 
 if(process.env.JAWSDB_URL){
-    connnection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else{
     connection = mysql.createConnection({
